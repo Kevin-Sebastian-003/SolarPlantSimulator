2d1f430b38b553422c3ce528d0e7a79e79b96fa6
kevin-sebastian-003 <kevinsebastian003@gmail.com>
2020-09-03 20:31:38 +0530

    first modification

--- src/plant/utils.py
+++ src/plant/utils.py
@@ -3,6 +3,7 @@ import time
 import requests
 from .models import Device, Plant, EndPoint, TargetUri
 from devices.inverter import Inverter
+from devices.weather_station import Weather_Station
 from multiprocessing import Process
 
 
@@ -47,5 +48,27 @@ def pump_data(plant_id, interval=60):
         # Till here the processing for inverters is done
         #  Repeat this block for other device types
         #  TargetUri type value differs for each type
+        ws_devices = Device.objects.filter(plant_id=plant_id, type=1, status='RUNNING')
+        ws_data = {"plant_id": plant_id, "ws_list": []}
+        for device in ws_devices:
+            ws_data['ws_list'].append(
+                Weather_station(
+                    plant_id=plant_id,
+                    block_id=device.block_id,
+                    room_id=device.room_id,
+                    ws_id=device.device_id,
+                ).generate_random_data())
+        ws_cloud_url = "http://%s%s" % (
+                        EndPoint.objects.filter(type=1)[0].endpoint,
+                        TargetUri.objects.filter(pk=1)[0].uri)
 
+        ws_local_url = "http://%s%s" %(
+                    EndPoint.objects.filter(type=2)[0].endpoint,
+                    TargetUri.objects.filter(pk=1)[0].uri)
+
+        try:
+            r = requests.post(url=ws_cloud_url, json=ws_data, timeout=1)
+            r = requests.post(url=ws_local_url, json=ws_data, timeout=1)
+        except Exception:
+            pass
         time.sleep(interval)
